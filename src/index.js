a0855d31a1d8244da70fb1b6a014791cde08e1bf
Aleksander Repin <alex.repin@mail.ru>
2017-12-19 00:23:06 +0400

    v0.0.4

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
-class InformSinkMail{
-    /**
+const Sink = require('not-inform').Sink,
+	log = require('not-log')(module),
+	nodemailer = require('nodemailer'),
+	hb = require('handlebars'),
+	wellknown = require('nodemailer-wellknown');
+
+class InformSinkMail extends Sink{
+	/**
      * @param {object} options
      */
-    constructor(options){
+	constructor(options){
+		super(options);
+		return this;
+	}
 
-    }
+	deploy(message){
+		let service = wellknown(this.options.service),
+    		transporter = nodemailer.createTransport(Object.assign({
+    			auth: this.options.auth
+    		}, service), {
+    			debug: process.env.NODE_ENV === 'development'
+    		}),
+    		html = hb.compile(this.options.templates.html),
+    		text = hb.compile(this.options.templates.text),
+    		subject = hb.compile(this.options.templates.subject),
+    		mailOptions = {
+    			from: this.options.from, 	// from email field
+    			to: message.to, 			// to email field
+    			subject: subject(message), 	// Subject line
+    			text: text(message), 		// Plain text body
+    			html: html(message)			// HTML version of email
+    		};
+    	transporter.sendMail(mailOptions, (error, info) => {
+    		if (error) {
+    			return log.error(error);
+    		}else{
+				log.info('Message sent: ' + info.response);
+			}
+    	});
+	}
 }
 
 module.exports = InformSinkMail;
