5bb1c95b8c569816a5796ba9cf4b206cbb9bfae0
Aleksander Repin <alex.repin@mail.ru>
2019-04-14 10:12:29 +0300

    added options.account support

--- src/index.js
+++ src/index.js
@@ -14,20 +14,27 @@ class InformSinkMail extends Sink{
 	}
 
 	deploy(message){
-		let service = wellknown(this.options.service),
+		let transporter;
+		if (this.options.service){
+			let service = wellknown(this.options.service);
 			transporter = nodemailer.createTransport(Object.assign({
 				auth: this.options.auth
 			}, service), {
 				debug: process.env.NODE_ENV === 'development'
-			}),
-			html = hb.compile(this.options.templates.html),
+			});
+		}else{
+			transporter = nodemailer.createTransport(this.options.account, {
+				debug: process.env.NODE_ENV === 'development'
+			});
+		}
+		let	html = hb.compile(this.options.templates.html),
 			text = hb.compile(this.options.templates.text),
 			subject = hb.compile(this.options.templates.subject),
 			mailOptions = {
-				from: 		this.options.from, 	// from email field
-				to: 		message.to, 			// to email field
-				subject: 	subject(message), 	// Subject line
-				text: 		text(message), 		// Plain text body
+				from: 		this.options.from, 		// from email field
+				to: 			message.to, 			// to email field
+				subject: 	subject(message), 		// Subject line
+				text: 		text(message), 			// Plain text body
 				html: 		html(message)			// HTML version of email
 			};
 		transporter.sendMail(mailOptions, (error, info) => {
