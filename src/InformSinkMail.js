92d5c7ba221cabe351a7ebc74102d55180b3b81d
interrupter <alex.repin@mail.ru>
2021-05-19 19:19:39 +0300

    added: support of owner/ownerModel fields in message to determine target email address field

--- src/InformSinkMail.js
+++ src/InformSinkMail.js
@@ -1,4 +1,5 @@
-const log = require('not-log')(module);
+const log = require('not-log')(module, 'notInformSinkEmail'),
+	validator = require('validator')
 try{
 	const Sink = require('not-inform').Sink,
 		nodemailer = require('nodemailer'),
@@ -14,48 +15,77 @@ try{
 			return this;
 		}
 
-		deploy(message, rule){
-			let transporter,
-				options = {
-					account: this.options.account
-				};
-			if(rule && rule.getData()){
-				options = Object.assign(options, rule.getData());
-			}
-			if (options.account.service){
-				let service = wellknown(options.account.service);
-				transporter = nodemailer.createTransport(Object.assign({
-					auth: options.account.auth
-				}, service), {
-					debug: process.env.NODE_ENV === 'development'
-				});
+		async getEmailTo(message){
+			if(
+				Object.prototype.hasOwnProperty.call(message, 'to') &&
+				validator.isEmail(message.to)
+			){
+				return message.to;
 			}else{
-				transporter = nodemailer.createTransport(options.account, {
-					debug: process.env.NODE_ENV === 'development'
-				});
-			}
-			let	html = hb.compile(options.templates.html),
-				text = hb.compile(options.templates.text),
-				subject = hb.compile(options.templates.subject),
-				mailOptions = {
-					from: 		options.from, 		// from email field
-					to: 			message.to, 			// to email field
-					subject: 	subject(message), 		// Subject line
-					text: 		text(message), 			// Plain text body
-					html: 		html(message)			// HTML version of email
-				};
-			transporter.sendMail(mailOptions, (error, info) => {
-				if (error) {
-					return log.error(error);
-				}else{
-					log.info('Message sent: ' + info.response);
+				if(
+					Object.prototype.hasOwnProperty.call(message, 'owner') &&
+					Object.prototype.hasOwnProperty.call(message, 'ownerModel')
+				){
+					let model = notNode.Application.getModel(message.ownerModel);
+					let owner = await model.getOne(message.owner);
+					if(owner.email && validator.isEmail(owner.email)){
+						return owner.email;
+					}
 				}
-			});
+			}
+			return false;
+		}
+
+		async deploy(message, rule){
+			try{
+				let transporter,
+					emailTo = await this.getEmailTo(message),
+					options = {
+						account: this.options.account
+					};
+				if(!emailTo){
+					throw new Error('No valid email or owner of valid email provided');
+				}
+				if(rule && rule.getData()){
+					options = Object.assign(options, rule.getData());
+				}
+				if (options.account.service){
+					let service = wellknown(options.account.service);
+					transporter = nodemailer.createTransport(Object.assign({
+						auth: options.account.auth
+					}, service), {
+						debug: process.env.NODE_ENV === 'development'
+					});
+				}else{
+					transporter = nodemailer.createTransport(options.account, {
+						debug: process.env.NODE_ENV === 'development'
+					});
+				}
+				let	html = hb.compile(options.templates.html),
+					text = hb.compile(options.templates.text),
+					subject = hb.compile(options.templates.subject),
+					mailOptions = {
+						from: 		options.from, 		// from email field
+						to: 			emailTo, 			// to email field
+						subject: 	subject(message), 		// Subject line
+						text: 		text(message), 			// Plain text body
+						html: 		html(message)			// HTML version of email
+					};
+				transporter.sendMail(mailOptions, (error, info) => {
+					if (error) {
+						return log.error(error);
+					}else{
+						log.info('Message sent: ' + info.response);
+					}
+				});
+			}catch(e){
+				log.error(e);
+			}
 		}
 	}
 
 	module.exports = InformSinkMail;
-	
+
 }catch(e){
 	log.error(e);
 }
