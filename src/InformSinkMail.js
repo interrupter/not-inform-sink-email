a6c412a316f0f7e740d4aa6526c8042ea59a4ad8
interrupter <alex.repin@mail.ru>
2021-05-20 20:26:58 +0300

    added: support for new Sink.deployCycle/One

--- src/InformSinkMail.js
+++ src/InformSinkMail.js
@@ -16,72 +16,66 @@ try{
 			return this;
 		}
 
-		async getEmailTo(message){
+		getEmailTo(recipient){
 			if(
-				Object.prototype.hasOwnProperty.call(message, 'to') &&
-				validator.isEmail(message.to)
+				Object.prototype.hasOwnProperty.call(recipient, 'email') &&
+				validator.isEmail(recipient.email)
 			){
-				return message.to;
+				return recipient.email;
 			}else{
-				if(
-					Object.prototype.hasOwnProperty.call(message, 'owner') &&
-					Object.prototype.hasOwnProperty.call(message, 'ownerModel')
-				){
-					let model = notNode.Application.getModel(message.ownerModel);
-					let owner = await model.getOne(message.owner);
-					if(owner.email && validator.isEmail(owner.email)){
-						return owner.email;
-					}
-				}
+				return false;
 			}
-			return false;
 		}
 
 		async deploy(message, rule){
 			try{
-				let transporter,
-					emailTo = await this.getEmailTo(message),
-					options = {
-						account: this.options.account
-					};
-				if(!emailTo){
-					throw new Error('No valid email or owner of valid email provided');
-				}
-				if(rule && rule.getData()){
-					options = Object.assign(options, rule.getData());
-				}
-				if (options.account.service){
-					let service = wellknown(options.account.service);
-					transporter = nodemailer.createTransport(Object.assign({
-						auth: options.account.auth
-					}, service), {
-						debug: process.env.NODE_ENV === 'development'
-					});
-				}else{
-					transporter = nodemailer.createTransport(options.account, {
-						debug: process.env.NODE_ENV === 'development'
-					});
-				}
-				let	html = hb.compile(options.templates.html),
-					text = hb.compile(options.templates.text),
-					subject = hb.compile(options.templates.subject),
-					mailOptions = {
-						from: 		options.from, 		// from email field
-						to: 			emailTo, 			// to email field
-						subject: 	subject(message), 		// Subject line
-						text: 		text(message), 			// Plain text body
-						html: 		html(message)			// HTML version of email
-					};
-				transporter.sendMail(mailOptions, (error, info) => {
-					if (error) {
-						return log.error(error);
-					}else{
-						log.info('Message sent: ' + info.response);
-					}
-				});
+				await this.deployCycle(message, rule);
 			}catch(e){
 				log.error(e);
 			}
+		}
+
+		async deployOne({message, recipient,  index, recipientsFilter, rule}){
+			let transporter,
+				emailTo = this.getEmailTo(recipient),
+				options = {
+					account: this.options.account
+				};
+			if(!emailTo){
+				throw new Error('No valid email for recipient');
+			}
+			if(rule && rule.getData()){
+				options = Object.assign(options, {...rule.getData()});
+			}
+			if (options.account.service){
+				let service = wellknown(options.account.service);
+				transporter = nodemailer.createTransport(Object.assign({
+					auth: options.account.auth
+				}, service), {
+					debug: process.env.NODE_ENV === 'development'
+				});
+			}else{
+				transporter = nodemailer.createTransport(options.account, {
+					debug: process.env.NODE_ENV === 'development'
+				});
+			}
+			let	html = hb.compile(options.templates.html),
+				text = hb.compile(options.templates.text),
+				subject = hb.compile(options.templates.subject),
+				mailOptions = {
+					from: 		options.from, 		// from email field
+					to: 			emailTo, 			// to email field
+					subject: 	subject(message), 		// Subject line
+					text: 		text(message), 			// Plain text body
+					html: 		html(message)			// HTML version of email
+				};
+			transporter.sendMail(mailOptions, (error, info) => {
+				if (error) {
+					return log.error(error);
+				}else{
+					log.info('Message sent: ' + info.response);
+				}
+			});
 		}
 	}
 
